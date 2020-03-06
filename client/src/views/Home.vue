d886a718155713cab82a1cdc05f8481aab08415a
Dieter Heidrich <dnbheidrich@gmail.com>
2020-03-05 22:15:04 -0700

    css changes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -8,7 +8,7 @@
           >Log-in to acess starboard </h3>
           <h3 class="text-primary" v-else>Welcome manage your starboard!</h3>
     <img class=".noAuthImg" v-if="!$auth.isAuthenticated" src="https://s3.amazonaws.com/cms.ipressroom.com/295/files/20185/5b1f7a1b9dee811c49d518ab_Omega562Stars_lo/Omega562Stars_lo_d44bc804-72b3-4b7a-b7ca-94d9a4780bfd-prv.jpg" alt />
-    <img class="authImg"  v-else src="https://assets.newatlas.com/dims4/default/4176a07/2147483647/strip/true/crop/1350x1080+0+0/resize/1200x960!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2Forion-manned-1.jpg" alt />
+    <img class="authImg"  v-else src="https://lh3.googleusercontent.com/proxy/oZfy1X_1LKOHE5vPftZIA0sSJRApM1p4tV72jnOW20CZlNndazhY7px2tLksDVPBslgGymwHH93FoSyM5hRdnTbjLWcbAuFMDWxNph2SsUMwo0-MLQLjnYF5xkn_XJVHgQZ_rrUboYCuhugVEq5aLeO8-Ep6nH-mpl16OXUnS1EL4mnVygkUktnv" alt />
       </div>
     </div>
   </div>
@@ -41,13 +41,13 @@ export default {
   width: 100;
 }
 .authImg{
-   height: 25%;
+   height: 75%;
   width: 100;
   position: center;
 }
 .noAuthImg{
    height: 25%;
-  width: 50;
+  width: 25;
   position: center;
 
 }
